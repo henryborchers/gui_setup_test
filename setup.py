a7a8cd02bec507e5325a611e98be7f55339486a6
henryborchers <henryborchers@yahoo.com>
2015-06-24 13:29:05 -0700

    added test case

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gui_setup_test',
-    version='0.1.3',
+    version='0.1.4',
     packages=['tester'],
     url='',
     license='',
--- tester/testGui.py
+++ tester/testGui.py
@@ -5,12 +5,22 @@ if sys.version_info > (3,0):
 else:
     from Tkinter import *
 def main():
-    print("Hello")
-    root = Tk()
-    # w = tkinter.Label(root, text="Hello World")
-    w = Button(root, text="Close Me", command=callback)
-    w.pack()
-    root.mainloop()
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "--test":
+            print("Test successful")
+            exit()
+        else:
+            exit(-1)
+    if len(sys.argv) > 2:
+        exit()
+    elif len(sys.argv) == 1:
+        print("Hello")
+        root = Tk()
+        # w = tkinter.Label(root, text="Hello World")
+        w = Button(root, text="Close Me", command=callback)
+        w.pack()
+        root.mainloop()
 
 def callback():
     print("Closing")
